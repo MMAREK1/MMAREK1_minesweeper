5dcf8633f237a14574afdb70b36944ee99bb3179
MMAREK1 <martin.mat.marek@gmail.com>
2015-07-30 16:22:03 +0200

    task 1 and Task 2

--- src/minesweeper/consoleui/ConsoleUI.java
+++ src/minesweeper/consoleui/ConsoleUI.java
@@ -36,15 +36,15 @@ public class ConsoleUI {
         do {
             update();
             processInput();
-            throw new UnsupportedOperationException("Resolve the game state - winning or loosing condition.");
-        } while(true);
+            //throw new UnsupportedOperationException("Resolve the game state - winning or loosing condition.");
+        } while(false);
     }
     
     /**
      * Updates user interface - prints the field.
      */
     public void update() {
-        throw new UnsupportedOperationException("Method update not yet implemented");
+        //throw new UnsupportedOperationException("Method update not yet implemented");
     }
     
     /**
@@ -52,6 +52,6 @@ public class ConsoleUI {
      * Reads line from console and does the action on a playing field according to input string.
      */
     private void processInput() {
-        throw new UnsupportedOperationException("Method processInput not yet implemented");
+        //throw new UnsupportedOperationException("Method processInput not yet implemented");
     }
 }
--- src/minesweeper/core/Mine.java
+++ src/minesweeper/core/Mine.java
@@ -4,4 +4,13 @@ package minesweeper.core;
  * Mine tile.
  */
 public class Mine extends Tile {
+	
+    @Override
+    public String toString() {
+    	// TODO Auto-generated method stub
+		if (this.getState() == State.OPEN)
+			return "* ";
+		else
+			return super.toString();
+    }
 }
